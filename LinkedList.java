affd52e37ff0abe8801e561ae0835f08e777f73d
BigM66 <112117789+BigM66@users.noreply.github.com>
2022-09-07 20:57:13 -0400

    this is the Assignment 2 generic linked list code

--- LinkedList.java
+++ LinkedList.java
@@ -1,33 +1,59 @@
-/**
- * 
- */
+
 
 /**
- * @author mpars
+ * a class that creates a generic linked list 
+ * @author Matthew Parsley
  *
  */
 
 
 public class LinkedList<T> extends Node<T> {
-	
+	/*
+	 * creates the head of the Node
+	 */
 	Node head;
+	/*
+	 * creates an integer to hold the length of the node 
+	 */
 	int length;
+	/*
+	 * creates the tail of the node 
+	 */
 	Node tail;
+	
+	/*
+	 * sets up our linkedlist
+	 */
 	public LinkedList()
 	{
 		head=null;
 		length=0;
 		tail=head;
 	}
+	
+	/*
+	 * determines if the linked list is empty
+	 * @returns true if it is false if not 
+	 */
 	public boolean isEmpty()
 	{
 		if (head==null) { return true;}
 		else return false;
 	}
+	
+	/*
+	 * tells us the value of the first item in our linked list 
+	 * @returns the value of the first node in the linked list 
+	 */
 	public Node getList()
 	{
 		return head;
 	}
+	
+	/*
+	 * adds another node to the end of the linked list 
+	 * @param aNode- the node we are adding 
+	 */
 	public void addNode(Node aNode)
 	{
 		if (isEmpty() ) {
--- Node.java
+++ Node.java
@@ -1,21 +1,56 @@
+/**
+ * the class for creating the node 
+ * @author Matthew Parsley
+ *
+ */
+
 
 class Node <T> {
+	/*
+	 * creates a generic item called data 
+	 */
 	T data;
+	
+	/*
+	 * sets up a node to be useable as a the next node in a list
+	 */
 	Node<T> nextNode;
+	
+	/*
+	 * creates our node item
+	 */
 	public Node()
 	{
 		nextNode=null;
 	}
+	
+	/*
+	 * sets our data to be the type we want it to be
+	 * @param the type of data of our choosing 
+	 */
 	public void setData(T data) {
 		this.data=data;
 	}
+	
+	/*
+	 * gets the value assigned to the node 
+	 * @returns the value of the node 
+	 */
 	public Node<T> getNode() {
 		return this;
 	}
+	
+	/*
+	 * makes it to where we can add another node to our list 
+	 * @param the name of the next node we create 
+	 */
 	public void setNextNode(Node<T> nextNode)
 	{
 		this.nextNode=nextNode;
 	}
 	
 	
-}
+} 
+/*
+ * end of class
+ */
